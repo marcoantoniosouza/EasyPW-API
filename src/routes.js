860cd9a423a0382ee50ea98818ae3e50c7bccac5
Marco Antonio Souza <maps@ic.ufal.br>
2020-06-30 00:01:44 -0300

    change pass update http verb

--- src/routes.js
+++ src/routes.js
@@ -7,13 +7,13 @@ const routes = express.Router();
 routes.get("/hello", (req, res) => {return res.json({h:"hello"});});
 
 const user = new Users();
-routes.get("/users", user.index);
+routes.get ("/users", user.index);
 routes.post("/users", user.create);
 routes.post("/login", user.login);
 
 const pass = new Passes();
-routes.get("/passes", pass.index);
+routes.get ("/passes", pass.index);
 routes.post("/passes", pass.create);
-routes.post("/pass", pass.update);
+routes.put ("/passes", pass.update);
 
 module.exports = routes;
