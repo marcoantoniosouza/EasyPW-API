9039e3ec37a7c77bd8187fa6bf0e9674aab1a7ec
Marco Antonio Souza <maps@ic.ufal.br>
2020-06-29 23:26:21 -0300

    Create index unique user

--- src/Model/Users.js
+++ src/Model/Users.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 
 const Model = conn.model('User', {
     nome: String,
-    user: String,
+    user: {type: String, unique: true},
     pass: String,
     authHash: String
 });
@@ -20,15 +20,22 @@ class User{
 
         const reqBody = request.body;
         const pass = hash.update(reqBody.pass).digest('hex');
-        const authHash = hashAuth.update(reqBody.user + pass + 'PaulaPass@)@)').digest('hex')
+        const authHash = hashAuth.update(reqBody.user + pass + 'PaulaPass@)@)').digest('hex');
         const userJson = {
             nome: reqBody.nome,
             user: reqBody.user,
             pass: pass,
             authHash: authHash,
+        };
+        try {
+            await Model.create(userJson);
+            return response.json({status: "OK!"});
+        } catch (error) {
+            response.status(400);
+            return response.json({message: 'Usuário indisponível'});
         }
-        await Model.create(userJson);
-        return response.json({Status: "OK!"});
+        
+        
     }
 
     async login(request, response){
